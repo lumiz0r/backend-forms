a53ee8189152289347aae7d19f29be4941f38f20
Christian Cuesta <christianluminor@gmail.com>
2023-10-25 17:11:17 +0200

    Check if id already exists

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import {User} from "./models.js";
+import { User } from "./models.js";
 
 const resolvers = {
   Query: {
@@ -7,11 +7,19 @@ const resolvers = {
   },
   Mutation: {
     createUser: async (_, args) => {
+      const existingUser = await User.findOne({ id: args.id });
+
+      if (existingUser) {
+        throw new Error("User with this ID already exists.");
+      }
+
       const user = new User(args);
+
       await user.save();
+
       return user;
     },
-  }
+  },
 };
 
 export default resolvers;
